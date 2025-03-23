e3611d2e8265faa2f4576fa9961564a31cb7bf56
Yugyeong Park <bonbonly7@naver.com>
2025-03-23 14:46:39 +0900

    feature: Book 대여 기본 기능 틀 구현

---
+++ src/main/java/org/be/book/repository/RentalRepository.java
@@ -0,0 +1,11 @@
+package org.be.book.repository;
+
+import org.be.book.model.Rental;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface RentalRepository extends JpaRepository<Rental, Long> {
+    List<Rental> findByUserId(Long userId);
+}
