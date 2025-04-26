6dcbccf76563806b55fa7b2e22bb93f012828511
Yugyeong Park <bonbonly7@naver.com>
2025-04-26 21:03:17 +0900

    delete: 사용하지 않는 redisTemplateBook 삭제

--- src/main/java/org/be/recommend/config/RedisConfig.java
+++ src/main/java/org/be/recommend/config/RedisConfig.java
@@ -11,8 +11,6 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
-import java.util.List;
-
 @Configuration
 public class RedisConfig {
 
@@ -30,17 +28,6 @@ public class RedisConfig {
     @Bean
     public RedisTemplate<String, RecommendResponse> redisTemplateRecommendResponse(RedisConnectionFactory connectionFactory) {
         RedisTemplate<String, RecommendResponse> template = new RedisTemplate<>();
-        template.setConnectionFactory(connectionFactory);
-
-        template.setKeySerializer(new StringRedisSerializer());
-        template.setValueSerializer(new GenericJackson2JsonRedisSerializer(objectMapper()));
-
-        return template;
-    }
-
-    @Bean
-    public RedisTemplate<String, List<Book>> redisTemplateBook(RedisConnectionFactory connectionFactory) {
-        RedisTemplate<String, List<Book>> template = new RedisTemplate<>();
         template.setConnectionFactory(connectionFactory);
 
         template.setKeySerializer(new StringRedisSerializer());
