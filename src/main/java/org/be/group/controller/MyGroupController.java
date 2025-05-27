1ec5434fe8c73e51cbab8cb8b44c3e16c281bdb6
eunii2 <chb7562@gmail.com>
2025-05-27 21:36:58 +0900

    fix : group 부분 authorization header 형식으로 다시 변경

--- src/main/java/org/be/group/controller/MyGroupController.java
+++ src/main/java/org/be/group/controller/MyGroupController.java
@@ -5,6 +5,7 @@ import org.be.auth.service.CustomUserDetails;
 import org.be.group.dto.GroupResponseDto;
 import org.be.group.service.GroupMemberService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,6 +22,9 @@ class MyGroupController {
 
     @GetMapping
     public ResponseEntity<List<GroupResponseDto>> getMyGroups(@AuthenticationPrincipal CustomUserDetails userDetails) {
+        if (userDetails == null) {
+            throw new AccessDeniedException("로그인이 필요합니다.");
+        }
         return ResponseEntity.ok(groupMemberService.getMyGroups(userDetails.getUser()));
     }
 }
